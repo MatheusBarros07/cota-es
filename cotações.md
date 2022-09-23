046e6c0dbbd5dbea09eb1e2bb86aa9fcc73442a5
matheus Barros <mgama868@gmail.com>
2022-09-23 16:43:33 -0300

    Cotações

---
+++ cotações.md
@@ -0,0 +1,2 @@
+neste algoritmo eu pego cotações em tempo real 
+das moedas Dólar, Euro e BTC.
---
+++ cotações.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def pegar_cotacoes():
+    requisicao = requests.get("http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL")
+    requisicao_dic = requisicao.json()
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_bct = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    Dólar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_bct}'''
+    print(texto)
+
+
+pegar_cotacoes()
